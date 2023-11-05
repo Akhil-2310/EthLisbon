de11cb5d9ce3ab4fe186be85eac89c75c348247b
Akhil-2310 <akhilnanavati23@gmail.com>
2023-11-05 17:08:26 +0530

    final

--- Charity-DAO/src/Blockchain.services.jsx
+++ Charity-DAO/src/Blockchain.services.jsx
@@ -1,6 +1,6 @@
 import Web3 from "web3";
 import { setGlobalState, getGlobalState } from "./store";
-import abi from "./abis/CharityDAO.json";
+import abi from "./abis/CharityDao.json";
 
 const { ethereum } = window;
 window.web3 = new Web3(ethereum);
